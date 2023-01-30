de096fb3129f6aa3c5a1f7b5cd3d1d6cd9c83f4c
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2023-01-30 23:44:26 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,8 +4,8 @@ class Config:
     API_ID = int(getenv("API_ID", "1923471"))
     API_HASH = getenv("API_HASH", "fcdc178451cd234e63faefd38895c991")
     BOT_TOKEN = getenv("BOT_TOKEN", "5796564520:AAGU5KcNB1xowytuOOGuSx4TWz8jpibzXdo")
-    FSUB = getenv("FSUB", "")
-    CHID = int(getenv("CHID", ""))
+    FSUB = getenv("FSUB", "AsuranMoviefinder1")
+    CHID = int(getenv("CHID", "-1001702935595")
     SUDO = list(map(int, getenv("SUDO", "880087645").split()))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://Erichdaniken:Erichdaniken@cluster0.vhu3d.mongodb.net/?retryWrites=true&w=majority")
     
